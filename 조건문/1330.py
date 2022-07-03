015ec06e9a2819b13706b6ad23cc056fe81740c3
cjy3458 <cjy3458@naver.com>
2022-07-03 20:43:06 +0900

    조건문

---
+++ 조건문/1330.py
@@ -0,0 +1,11 @@
+a, b = input().split(" ")
+
+a = int(a)
+b = int(b)
+
+if a > b:
+    print(">")
+if a < b:
+    print("<")
+if a == b:
+    print("==")
---
+++ 조건문/9498.py
@@ -0,0 +1,12 @@
+test = int(input())
+
+if 90 <= test and test<= 100:
+    print("A")
+elif 80 <= test and test <= 89:
+    print("B")
+elif 70 <= test and test <= 79:
+    print("C")
+elif 60 <= test and test <= 69:
+    print("D")
+else:
+    print("F")

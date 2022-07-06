4e5e9647b6500a2dcab2ab0be2eeb19e29742f90
cjy3458 <cjy3458@naver.com>
2022-07-06 19:51:36 +0900

    반복문

---
+++ 단계별 문제풀이/반복문/2438.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+result = ""
+
+for i in range(1, t+1):
+    result += "*"
+    print(result)
---
+++ 단계별 문제풀이/반복문/2439.py
@@ -0,0 +1,9 @@
+t = int(input())
+
+result = ""
+
+for i in range(1, t+1):
+    result += "*"
+    print(" "*(t-i) + result)  
+    
+

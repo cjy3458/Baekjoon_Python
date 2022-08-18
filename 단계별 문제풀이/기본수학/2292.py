0737d0bfaca582bb199ba12260290255dbde6c68
cjy3458 <cjy3458@naver.com>
2022-08-18 15:39:27 +0900

    기본수학

--- 단계별 문제풀이/기본수학/2292.py
+++ 단계별 문제풀이/기본수학/2292.py
@@ -1,5 +1,19 @@
+def isSix(a):
+    num = 1
+    count = 1
+    while a > num:
+        num += 6*count
+        count += 1
+    return count
+
+
 n = int(input())
 
+
+
+print(isSix(n))
+
+'''
 num = 1
 count = 1
 
@@ -8,3 +22,4 @@ while n > num:
     count += 1
 
 print(count)
+'''
